8c5c7d47b10cc5cec17324a02d3632fb6ecb5ec5
AdrianRoLe <adrian.role.business@gmail.com>
2023-08-17 00:07:42 +0200

    Otimizing?

--- maths.js
+++ maths.js
@@ -1,10 +1,10 @@
-const Iterations_map = new Map();
+const iterations_map = new Map();
 
 const add_iteration = (number_of_iterations) => {
-	if (Iterations_map.has(number_of_iterations)) {
-		Iterations_map.set(number_of_iterations, Iterations_map.get(number_of_iterations) + 1);
+	if (iterations_map.has(number_of_iterations)) {
+		iterations_map.set(number_of_iterations, iterations_map.get(number_of_iterations) + 1);
 	} else {
-		Iterations_map.set(number_of_iterations, 1);
+		iterations_map.set(number_of_iterations, 1);
 	}
 }
 
@@ -76,12 +76,51 @@ const recursive = (number, counter) => {
 	}
 }
 
+const optimize_recursive = (number, counter, invalid_numbers) => {
+	counter++;
+	const contains = invalid_numbers.includes(number);
+	if (contains) {
+		return counter;
+	}
+	return optimize_recursive(apply_rules(number), counter, invalid_numbers);
+}
 
-const test = () => {
-	const max_numers = [100, 1000, 10000, 100000, 1000000, 10000000]
-	for (let j = 0; j < max_numers.length; j++) {
-		Iterations_map.clear();
-		for (let i = 1; i <= max_numers[j]; i++) {
+const optimize_recursive_divide = (number, counter, invalid_numbers) => {
+	counter++;
+	const contains = find_in_array(number, invalid_numbers)
+	if (contains) {
+		return counter;
+	}
+	return optimize_recursive_divide(apply_rules(number), counter, invalid_numbers);
+}
+
+
+const find_in_array = (number, array) => {
+	// find in array sorted, by dividing the array in two and checking if the number is bigger or smaller than the middle number
+
+	if (array.length == 0) {
+		return false;
+	}
+	if (array.length == 1) {
+		return number == array[0];
+	}
+	const middle = Math.floor(array.length / 2);
+	if (number < array[middle]) {
+		return find_in_array(number, array.slice(0, middle));
+	}
+	if (number > array[middle]) {
+		return find_in_array(number, array.slice(middle, array.length));
+	}
+	if (number == array[middle]) {
+		return true;
+	}
+}
+
+const generate_graphs = () => {
+	const max_numbers = [100, 1000, 10000, 100000, 1000000, 10000000]
+	for (let j = 0; j < max_numbers.length; j++) {
+		iterations_map.clear();
+		for (let i = 1; i <= max_numbers[j]; i++) {
 			try {
 				const iterations = recursive(i, 0);
 				add_iteration(iterations);
@@ -91,25 +130,73 @@ const test = () => {
 			}
 		}
 		// graph the map with Chart.js, as bar chart
-		const datasets = convert_map_to_bar_chart(Iterations_map);
+		const datasets = convert_map_to_bar_chart(iterations_map);
 		new Chart(document.getElementById("grid-" + (j + 1)), {
 			type: 'bar',
 			data: {
-				labels: Array.from(Iterations_map.keys()),
+				labels: Array.from(iterations_map.keys()),
 				datasets: [datasets]
 			},
 			options: {
 				legend: { display: false },
 				title: {
 					display: true,
-					text: 'Iterations for numbers up to ' + max_numers[j]
+					text: 'Iterations for numbers up to ' + max_numbers[j]
 				},
 			}
 		});
 	}
-
-
-
 }
 
-test();
+const generate_optimized_graph = () => {
+	const invalid_numbers = [1];
+	iterations_map.clear();
+	const max_number = 10000;
+	const start_time = new Date().getTime();
+	for (let i = 1; i < max_number; i++) {
+		try {
+			const iterations = optimize_recursive(i, 0, invalid_numbers);
+			add_iteration(iterations);
+			invalid_numbers.push(i);
+			invalid_numbers.sort((a, b) => a - b);
+
+			// console.log("\nNumber:" + i + " | Iterations: " + iterations);
+		} catch (error) {
+			console.log("\nError on: " + i + " | " + error);
+		}
+	}
+	const end_time = new Date().getTime();
+
+	const datasets = convert_map_to_bar_chart(iterations_map);
+	new Chart(document.getElementById("grid-opt"), {
+		type: 'bar',
+		data: {
+			labels: Array.from(iterations_map.keys()),
+			datasets: [datasets]
+		},
+		options: {
+			legend: { display: false },
+			title: {
+				display: true,
+				text: 'Iterations for numbers up to ' + max_number
+			},
+		}
+	});
+
+	const percentage_container = document.getElementById("percentage_container");
+	const percentage_span = document.getElementById("percentage");
+	const percentage = datasets.data[1] / max_number * 100.000;
+	percentage_span.innerHTML = percentage.toFixed(3) + "%";
+
+	const performance_includes = document.getElementById("performance_includes");
+	percentage_container.style.display = "block";
+}
+
+// ENTRY POINT
+
+document.addEventListener("DOMContentLoaded", function () {
+	document.getElementById("generate_graphs").addEventListener("click", generate_graphs);
+	document.getElementById("generate_optimized_graph").addEventListener("click", generate_optimized_graph);
+});
+
+
